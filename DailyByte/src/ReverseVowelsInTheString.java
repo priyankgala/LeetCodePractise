519d1a35472ad8fbf4cdb68ac0f7369644764c1f
Galapriyank <gala.pr@husky.neu.edu>
2021-02-28 16:32:53 -0500

    Reverse Vowels in a String

--- DailyByte/src/ReverseVowelsInTheString.java
+++ DailyByte/src/ReverseVowelsInTheString.java
@@ -1,36 +1,37 @@
+
 public class ReverseVowelsInTheString {
-    public static void main(String args[]){
-        System.out.println("Reversed string looks like this: "+reverseVowel("LeetCode"));
+
+    public static void main(String args[]) {
+        System.out.println("Reversed string looks like this: " + reverseVowel("LeetCode"));
     }
+
     // utility function to check for vowel 
-    static boolean isVowel(char c) { 
+    static boolean isVowel(char c) {
         return (c == 'a' || c == 'A' || c == 'e'
                 || c == 'E' || c == 'i' || c == 'I'
                 || c == 'o' || c == 'O' || c == 'u'
-                || c == 'U'); 
-    } 
-  
+                || c == 'U');
+    }
+
 // Function to reverse order of vowels 
-    static String reverseVowel(String str1) { 
-        int j = 0; 
+    static String reverseVowel(String str1) {
         // Storing the vowels separately 
-        char[] str = str1.toCharArray(); 
-        String vowel = ""; 
-        for (int i = 0; i < str.length; i++) { 
-            if (isVowel(str[i])) { 
-                j++; 
-                vowel += str[i]; 
-            } 
-        } 
-  
-        // Placing the vowels in the reverse 
-        // order in the string 
-        for (int i = 0; i < str.length; i++) { 
-            if (isVowel(str[i])) { 
-                str[i] = vowel.charAt(--j); 
-            } 
-        } 
-  
-        return String.valueOf(str); 
-    } 
+        char[] str = str1.toCharArray();
+        int start = 0;
+        int end = str.length - 1;
+        while (start < end) {
+            if (isVowel(str[start]) && isVowel(str[end])) {
+                char c = str[start];
+                str[start++] = str[end];
+                str[end--] = c;
+            } else if (!isVowel(str[start])) {
+                start++;
+            }
+             else if (!isVowel(str[end])) {
+                end--;
+            }
+        }
+
+        return String.valueOf(str);
+    }
 }
