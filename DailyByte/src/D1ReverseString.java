73ada48dfb03e5ff952f2abffacd9fb9765c6860
Galapriyank <gala.pr@husky.neu.edu>
2021-02-28 15:42:26 -0500

    Reverse a string in Java

--- DailyByte/src/D1ReverseString.java
+++ DailyByte/src/D1ReverseString.java
@@ -1,3 +1,4 @@
+
 public class D1ReverseString {
 
     public static void main(String args[]) {
@@ -7,19 +8,28 @@ public class D1ReverseString {
         StringBuilder s = new StringBuilder();
         s.append(str);
         s.reverse();
-        System.out.println(""+s);
-                
-                
-        System.out.println(reverseString(str));
+//        System.out.println("" + s);
+
+//        System.out.println(reverseString(str));
+        System.out.println("Using string builder: "+reverseString1(str));
     }
 
     private static String reverseString(String str) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         StringBuilder s = new StringBuilder();
-        for(char c: str.toCharArray()){
+        for (char c : str.toCharArray()) {
             s.insert(0, c);
         }
         System.out.println(s);
         return s.toString();
+    }
+    
+    private static String reverseString1(String str) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+//        return new StringBuilder().append(str).reverse(str).toString();
+//        StringBuilder s = new StringBuilder(str);
+        
+        return new StringBuilder(str).reverse().toString();
     }
 }
